8aaa3f1b284665b1665a5daedf2d4c57ea3ccbb9
Annika <annika.meinecke@gmx.de>
2018-08-23 16:29:43 +0200

    Add choropleth, table and basics for interaction

--- choropleth.js
+++ choropleth.js
@@ -1,37 +1,143 @@
 Plotly.d3.csv('whr_data_vis.csv', function (err, rows) {
     function unpack(rows, key) {
-        return rows.map(function (row) { return row[key]; });
+        return rows.map(function (row) {
+            return row[key];
+        });
     }
     function get_text(rows) {
-        return rows.map(function (row) { return 'Happiness: ' + (row['Happiness score']).toString() + '<br>Dystopia: ' + (row['Dystopia']).toString() + '<br>GDP per capital: ' + (row['GDP']).toString() + '<br>Social support: ' + (row['Social support']).toString() + '<br>Healthy life expectancy: ' + (row['Healthy life expectancy']).toString() + '<br>Freedom to make life choices: ' + (row['Freedom to make life choices']).toString() + '<br>Generosity: ' + (row['Generosity']).toString() + '<br>Perceptions of corruption: ' + (row['Perceptions of corruption']).toString(); });
-    }
-my_text = get_text(rows);
-trace1 = {
-    type: 'choropleth',
-    locationmode: 'country names',
-    locations: unpack(rows, 'Country'),
-    z: unpack(rows, 'Happiness score'),
-    text: my_text,
-    hoverinfo: 'text+location',
-    autocolorscale: true,
+        texts = rows.map(function (row) {
+            if (row['HappinessScore'] <= threshold) { return 'Happiness: ' + (row['HappinessScore']).toString() + '<br>Dystopia: ' + (row['Dystopia']).toString() + '<br>GDP per capital: ' + (row['GDP']).toString() + '<br>Social Support: ' + (row['SocialSupport']).toString() + '<br>Healthy life expectancy: ' + (row['LifeExpectancy']).toString() + '<br>Freedom to make life choices: ' + (row['LifeChoices']).toString() + '<br>Generosity: ' + (row['Generosity']).toString() + '<br>Perceptions of corruption: ' + (row['Corruption']).toString(); }
+            else {
+                return 0;
+            }
+        });
+        return texts.filter(check_selected);
 
-    };
-data=[trace1];
-layout = {
-    title: 'World Happiness Report 2018',
-    geo: { 
-        projection: {
-        type: 'equirectangular'
+    }
+    function get_happiness(rows, value){
+
+        var result = rows.find(obj => {
+            return obj.Country === value;
+          })
+          return [[result.Country, result.HappinessScore],[result.SocialSupport, result.LifeExpectancy, result.LifeChoices, result.Generosity, result.Corruption]];
+    }
+    function unpack_selected(rows, key) {
+        values = rows.map(function (row) {
+            if (row['HappinessScore'] <= threshold) {
+                return row[key];
+            }
+            else {
+                return 0;
+            }
+        })
+        return values.filter(check_selected);
+    }
+    function check_selected(n) {
+        return n != 0;
+    }
+
+    var happy = [];
+    var threshold = 6.0;
+    var label = ['Dystopia', 'GDP', 'Social Support', 'Healthy life expectancy', 'Freedom to make life choices', 'Generosity', 'Perception of corruption']
+
+    // Create Data -> call function before calling Plotly.react to update data
+    function create_data() {
+        var headerNames = ['Country', 'HappinessScore']
+
+        var headerValues = [];
+        var cellValues = [];
+        for (i = 0; i < headerNames.length; i++) {
+            headerValue = [headerNames[i]];
+            headerValues[i] = headerValue;
+            cellValue = unpack_selected(rows, headerNames[i]);
+            cellValues[i] = cellValue;
         }
+
+        // clean date
+        for (i = 0; i < cellValues[1].length; i++) {
+            var dateValue = cellValues[1][i].split(' ')[0]
+            cellValues[1][i] = dateValue
+        }
+
+
+        var table = {
+            type: 'table',
+            columnwidth: [200, 100],
+            columnorder: [0, 1],
+            header: {
+                values: headerValues,
+                align: "center",
+                line: { width: 1, color: 'rgb(50, 50, 50)' },
+                fill: { color: ['rgb(235, 100, 230)'] },
+                font: { family: "Arial", size: 11, color: "white" }
+            },
+            cells: {
+                values: cellValues,
+                align: ["center", "center"],
+                line: { color: "black", width: 1 },
+                font: { family: "Arial", size: 10, color: ["black"] }
+            },
+            xaxis: 'x',
+            domain: { x: [0, 0.2] }
+        }
+
+        my_text = get_text(rows);
+        trace1 = {
+            type: 'choropleth',
+            mode: 'markers',
+            locationmode: 'country names',
+            locations: unpack_selected(rows, 'Country'),
+            z: unpack_selected(rows, 'HappinessScore'),
+            text: my_text,
+            hoverinfo: 'text+location',
+            autocolorscale: true,
+            xaxis: 'x',
+            colorbar: {
+                x: 1,
+            },
+            domain: { x: [0.2, 0.8] }
+
+        };
+
+        trace3 = {
+            type: 'scatterpolar',
+            r: []
+            
+        }
+
+        data = [table, trace1];
     }
-};
+    create_data();
+    layout = {
+        title: 'World Happiness Report 2018',
+        geo: {
+            projection: {
+                type: 'equirectangular'
+            },
 
-Plotly.plot('choropleth', data, layout, { showLink: false }).then(gd => {
-    gd.on('plotly_click', d => {
-        var pt = (d.points || [])[0]
-        // enter and update selection
-        console.log(pt)
+        },
+        table: {
 
+        }
+        /*chart: {
+            domain:{
+                x: [0, 0.20]
+            }
+        }*/
+    };
+
+    Plotly.plot('choropleth', data, layout, { showLink: false }).then(gd => {
+        gd.on('plotly_click', d => {
+            var pt = (d.points || [])[0]
+            // enter and update selection
+
+            happy = get_happiness(rows, pt.location);
+            console.log(happy);
+            create_data();
+            Plotly.react('choropleth', data, layout,{showLink:false})
+
+            }
+
+        );
     });
-});
 });
