242306f39b2dafff7ddaa80fc97d796363fc4096
Annika <annika.meinecke@gmx.de>
2018-08-25 18:20:24 +0200

    change data

--- choropleth.js
+++ choropleth.js
@@ -1,233 +1,152 @@
-
-Plotly.d3.csv('whr_data_vis.csv', function (err, rows) {
-    function unpack(rows, key) {
-        return rows.map(function (row) {
-            return row[key];
-        });
-    }
-    function get_text(rows) {
-        texts = rows.map(function (row) {
-            if (row['Dystopia'] <= dystopia 
-            && row['GDP'] <= gdp 
-            && row['SocialSupport'] <= social
-            && row['LifeExpectancy'] <= lifeExp
-            && row['LifeChoices'] <= lifeChoices
-            && row['Generosity'] <= generosity
-            && row['Corruption'] <= corruption) 
-            { return 'Happiness: ' + (row['HappinessScore']).toString() 
-                + '<br>Dystopia: ' + (row['Dystopia']).toString() 
-                + '<br>GDP per capital: ' + (row['GDP']).toString() 
-                + '<br>Social Support: ' + (row['SocialSupport']).toString() 
-                + '<br>Healthy life expectancy: ' + (row['LifeExpectancy']).toString() 
-                + '<br>Freedom to make life choices: ' + (row['LifeChoices']).toString() 
-                + '<br>Generosity: ' + (row['Generosity']).toString() 
-                + '<br>Perceptions of corruption: ' + (row['Corruption']).toString(); }
-            else {
-                return 0;
-            }
-        });
-        return texts.filter(check_selected);
-
-    }
-    function get_happiness(rows, value){
-
-        var result = rows.find(obj => {
-            return obj.Country === value;
-          })
-          return [[result.Country, result.HappinessScore],[result.Dystopia, result.GDP, result.SocialSupport, result.LifeExpectancy, result.LifeChoices, result.Generosity, result.Corruption]];
-    }
-    function unpack_selected(rows, key) {
-        values = rows.map(function (row) {
-            if (row['Dystopia'] <= dystopia 
-            && row['GDP'] <= gdp 
-            && row['SocialSupport'] <= social
-            && row['LifeExpectancy'] <= lifeExp
-            && row['LifeChoices'] <= lifeChoices
-            && row['Generosity'] <= generosity
-            && row['Corruption'] <= corruption) {
-                return row[key];
-            }
-            else {
-                return 0;
-            }
-        })
-        return values.filter(check_selected);
-    }
-    function check_selected(n) {
-        return n != 0;
-    }
-
-    var happy = [['select country', 0.0],[0.0,0.0,0.0,0.0,0.0,0.0,0.0]];
-    var dystopia = 3.0;
-    var gdp =3.0;
-    var social = 3.0;
-    var lifeExp = 3.0; 
-    var lifeChoices = 3.0; 
-    var generosity = 3.0; 
-    var corruption = 3.0;
-
-    var label = ['Dystopia', 
-    'GDP', 
-    'Social Support', 
-    'Life Expectancy', 
-    'Life Choices', 
-    'Generosity', 
+var label = ['Dystopia',
+    'GDP',
+    'Social Support',
+    'Life Expectancy',
+    'Life Choices',
+    'Generosity',
     'Corruption']
 
-    // Create Data -> call function before calling Plotly.react to update data
-    function create_data() {
-        var headerNames = ['Country', 'HappinessScore']
-
-        var headerValues = [];
-        var cellValues = [];
-        for (i = 0; i < headerNames.length; i++) {
-            headerValue = [headerNames[i]];
-            headerValues[i] = headerValue;
-            cellValue = unpack_selected(rows, headerNames[i]);
-            cellValues[i] = cellValue;
-        }
-
-        // clean date
-        for (i = 0; i < cellValues[1].length; i++) {
-            var dateValue = cellValues[1][i].split(' ')[0]
-            cellValues[1][i] = dateValue
-        }
+// Create Data -> call function before calling Plotly.react to update data
+function create_data() {
+    var countries = getColumn("Country");
+    var happiness = getColumn("HappinessScore");
 
 
-        var table = {
-            type: 'table',
-            columnwidth: [200, 100],
-            columnorder: [0, 1],
-            header: {
-                values: headerValues,
-                align: "center",
-                line: { width: 1, color: 'rgb(50, 50, 50)' },
-                fill: { color: ['rgb(235, 100, 230)'] },
-                font: { family: "Arial", size: 11, color: "white" }
-            },
-            cells: {
-                values: cellValues,
-                align: ["center", "center"],
-                line: { color: "black", width: 1 },
-                font: { family: "Arial", size: 10, color: ["black"] }
-            },
-            xaxis: 'x',
-            domain: { x: [0, 0.2] }
-        }
+    var headerNames = ['Country', 'HappinessScore']
+    var cellValues = [countries, happiness];
 
-        my_text = get_text(rows);
-        trace1 = {
-            type: 'choropleth',
-            mode: 'markers',
-            locationmode: 'country names',
-            locations: unpack_selected(rows, 'Country'),
-            z: unpack_selected(rows, 'HappinessScore'),
-            text: my_text,
-            hoverinfo: 'text+location',
-            autocolorscale: true,
-            xaxis: 'x',
-            colorbar: {
-                x: 1,
-            },
-            domain: { x: [0.2, 0.8] }
-
-        };
-
-        trace2 = {
-            type: 'scatterpolar',
-            r: happy[1], 
-            theta: label,
-            fill: 'toself', 
-            name: happy[0][1],
-            text: happy[0][0],
-            textposition: 'top',
-
-            
-        };
-
-        data = [table, trace1, trace2];
-        
+    var table = {
+        type: 'table',
+        columnwidth: [200, 100],
+        columnorder: [0, 1],
+        header: {
+            values: headerNames,
+            align: "center",
+            line: { wtable_dataidth: 1, color: 'rgb(50, 50, 50)' },
+            fill: { color: ['rgb(235, 100, 230)'] },
+            font: { family: "Arial", size: 11, color: "white" }
+        },
+        cells: {
+            values: cellValues,
+            align: ["center", "center"],
+            line: { color: "black", width: 1 },
+            font: { family: "Arial", size: 10, color: ["black"] }
+        },
+        xaxis: 'x',
+        domain: { x: [0, 0.2] }
     }
-    create_data();
-    layout = {
-        title: 'World Happiness Report 2018',
-        geo: {
-            projection: {
-                type: 'equirectangular'
-            },
 
+    my_text = hover_text;
+    trace1 = {
+        type: 'choropleth',
+        mode: 'markers',
+        locationmode: 'country names',
+        locations: countries,
+        z: happiness,
+        text: my_text,
+        hoverinfo: 'text+location',
+        autocolorscale: true,
+        xaxis: 'x',
+        colorbar: {
+            x: 1,
         },
-        table: {
+        domain: { x: [0.2, 0.8] }
 
-        },
-        polar: {
-            domain: {
-                x: [0.8, 0.95],
-                y: [0, 1]
-            },
-            radialaxis: {
-                visible: true,
-                range: [0, 3],
-                angle: 90,
-            }
-
-        }
-        
     };
 
-    Plotly.plot('choropleth', data, layout, { showLink: false }).then(gd => {
-        gd.on('plotly_click', d => {
-            var pt = (d.points || [])[0]
-            // enter and update selection
+    trace2 = {
+        type: 'scatterpolar',
+        r: [happy.Dystopia, happy.GDP, happy.SocialSupport, happy.LifeExpectancy, happy.LifeChoices, happy.Generosity, happy.Corruption],
+        theta: label,
+        fill: 'toself',
+        name: happy.Country,
+        text: happy.HappinessScore,
+        textposition: 'top',
 
-            happy = get_happiness(rows, pt.location);
-            
-            if (radar_selection.length <= 4){
-                console.log(radar_selection.includes(happy));
-            radar_selection.push(happy);
-            }
-            create_data();
-            create_radar(radar_selection);
-            Plotly.react('choropleth', data, layout,{showLink:false});
-            Plotly.react('radar', radar, layout,{showLink:false});
 
-            }
+    };
 
-        );
-    });
-    radar_selection = []
-    function create_radar(data){
-        radar = []; 
-        if(radar_selection.length == 0){
-            radarData = {
-                type: 'scatterpolar',
-                theta: label,
-                r: [0,0,0,0,0,0,0]
-            }
-            radar.push(radarData);
+    data = [table, trace1, trace2];
+
+}
+create_data();
+layout = {
+    title: 'World Happiness Report 2018',
+    geo: {
+        projection: {
+            type: 'equirectangular'
+        },
+
+    },
+    table: {
+
+    },
+    polar: {
+        domain: {
+            x: [0.8, 0.95],
+            y: [0, 1]
+        },
+        radialaxis: {
+            visible: true,
+            range: [0, 3],
+            angle: 90,
         }
 
-        for (i=0; i<data.length; i++){
-            radarData = {
-                type: 'scatterpolar',
-                r: data[i][1],
-                theta: label,
-                fill: 'toself', 
-                name: data[i][0][0],
-
-            };
-            radar.push(radarData);
-        }
-
-    
     }
-radar_empty = [['',0.0],[0,0,0,0,0,0,0]]; 
-radarData = [radar_empty,radar_empty,radar_empty,radar_empty]
-create_radar(radarData);
-    Plotly.plot('radar', radar, layout,{showLink:false}).then(gd => {
-        gd.on('plotly_click', d => {
-            var pt = (d.points || [])[0];
-        })
-    })
+
+};
+
+Plotly.plot('choropleth', data, layout, { showLink: false }).then(gd => {
+    gd.on('plotly_click', d => {
+        var pt = (d.points || [])[0]
+        // enter and update selection
+
+        happy = getHappiness(pt.location);
+
+        if (radar_selection.length <= 4) {
+            console.log(radar_selection.includes(happy));
+            radar_selection.push(happy);
+        }
+        create_data();
+        create_radar(radar_selection);
+        Plotly.react('choropleth', data, layout, { showLink: false });
+        Plotly.react('radar', radar, layout, { showLink: false });
+
+    }
+
+    );
 });
-console.log(whr_data);
+radar_selection = []
+function create_radar(data) {
+    radar = [];
+    if (radar_selection.length == 0) {
+        radarData = {
+            type: 'scatterpolar',
+            theta: label,
+            r: [0, 0, 0, 0, 0, 0, 0]
+        }
+        radar.push(radarData);
+    }
+
+    for (i = 0; i < data.length; i++) {
+        radarData = {
+            type: 'scatterpolar',
+            r: [data[i].Dystopia, data[i].GDP, data[i].SocialSupport, data[i].LifeExpectancy, data[i].LifeChoices, data[i].Generosity, data[i].Corruption],
+            theta: label,
+            fill: 'toself',
+            name: data[i].Country,
+
+        };
+        radar.push(radarData);
+    }
+
+
+}
+radar_empty = [['', 0.0], [0, 0, 0, 0, 0, 0, 0]];
+radarData = [radar_empty, radar_empty, radar_empty, radar_empty]
+create_radar(radarData);
+Plotly.plot('radar', radar, layout, { showLink: false }).then(gd => {
+    gd.on('plotly_click', d => {
+        var pt = (d.points || [])[0];
+    })
+})
--- data.js
+++ data.js
@@ -1,20 +1,108 @@
+var selected_data = [];
+var hover_text = [];
+var table_data = [];
+
+function select_data() {
+	filter_data = d[0];
+	selected_data = [];
+	hover_text = [];
+	table_data = [];
+	for (i = 0; i < data_complete.length; i++) {
+		current_data = data_complete[i];
+
+		if (current_data.Dystopia <= filter_data.skills[0].value
+			&& current_data.GDP <= filter_data.skills[1].value
+			&& current_data.SocialSupport <= filter_data.skills[2].value
+			&& current_data.LifeExpectancy <= filter_data.skills[3].value
+			&& current_data.LifeChoices <= filter_data.skills[4].value
+			&& current_data.Generosity <= filter_data.skills[5].value
+			&& current_data.Corruption <= filter_data.skills[6].value) {
+			selected_data.push(current_data);
+			table_data.push([current_data.Country, current_data.HappinessScore]);
+			hover_text.push('Happiness: ' + current_data.HappinessScore.toString()
+				+ '<br>Dystopia: ' + current_data.Dystopia.toString()
+				+ '<br>GDP per capital: ' + current_data.GDP.toString()
+				+ '<br>Social Support: ' + current_data.SocialSupport.toString()
+				+ '<br>Healthy life expectancy: ' + current_data.LifeExpectancy.toString()
+				+ '<br>Freedom to make life choices: ' + current_data.LifeChoices.toString()
+				+ '<br>Generosity: ' + current_data.Generosity.toString()
+				+ '<br>Perceptions of corruption: ' + current_data.Corruption.toString());
+		}
+	}
+	return selected_data;
+}
+
 var d = [
 
 	{
-		name:"Data Set",
-		color:'#a6cee3',
-		skills:[
-			{axis:"Dystopia",value:3.00},
-			{axis:"GDP",value:3.00},
-			{axis:"Social Support",value:3.00},
-			{axis:"Life Expectancy",value:3.00},
-			{axis:"Freedom",value:3.00},
-			{axis:"Generosity",value:3.00},
-			{axis:"Corruption",value:3.0},
-		  ],
+		name: "Data Set",
+		color: '#a6cee3',
+		skills: [
+			{ axis: "Dystopia", value: 3.00 },
+			{ axis: "GDP", value: 3.00 },
+			{ axis: "Social Support", value: 3.00 },
+			{ axis: "Life Expectancy", value: 3.00 },
+			{ axis: "Freedom", value: 3.00 },
+			{ axis: "Generosity", value: 3.00 },
+			{ axis: "Corruption", value: 3.0 },
+		],
 	}
 ];
 
+function getColumn(str) {
+	result = [];
+	for (i = 0; i < selected_data.length; i++) {
+		switch (str) {
+			case "Country":
+				result.push(selected_data[i].Country);
+				break;
+			case "HappinessScore":
+				result.push(selected_data[i].HappinessScore);
+				break;
+			case "Dystopia":
+				result.push(selected_data[i].Dystopia);
+				break;
+			case "GDP":
+				result.push(selected_data[i].GDP);
+				break;
+			case "SocialSupport":
+				result.push(selected_data[i].SocialSupport);
+				break;
+			case "LifeExpectancy":
+				result.push(selected_data[i].LifeExpectancy);
+				break;
+			case "LifeChoices":
+				result.push(selected_data[i].LifeChoices);
+				break;
+			case "Generosity":
+				result.push(selected_data[i].Generosity);
+				break;
+			case "Corruption":
+				result.push(selected_data[i].Corruption);
+				break;
+			default:
+				console.log("Error in fetching data")
+		}
+	}
+	return result;
 
+}
+var happy = [];
+function getHappiness(location) {
+	happy = [];
+
+	for (var i = 0; i < selected_data.length; i++) {
+		if (selected_data[i].Country == location) {
+			happy = selected_data[i];
+		}
+	}
+
+	return happy;
+
+
+}
+
+select_data();
+getColumn("Corruption");
 
 
